891f0c30e27fe9bcd94035a7ab68be01a479cad1
AdityaPanchal15 <AdityaPanchal15@users.noreply.github.com>
2024-11-04 11:09:19 +0000

    deploy: 245d1a9093975d54d9db3d6b28397134bf3cdd41

---
+++ _nuxt/BGjisaMG.js
@@ -0,0 +1,1 @@
+import{_ as t,v as c,t as e}from"./DrKM5M7f.js";const n={};function o(r,a){return e(),c("div",null,"Contact")}const _=t(n,[["render",o]]);export{_ as default};
--- _nuxt/DKbtuhni.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,v as c,t as e}from"./xpbUr92e.js";const n={};function o(r,a){return e(),c("div",null,"Contact")}const _=t(n,[["render",o]]);export{_ as default};
